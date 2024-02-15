60cff8212d743d2431ca9c713cab08c1ff210610
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-02-15 12:46:31 +0100

    add color slider to avoid repetition with sliders

--- lib/src/presentation/color_picker.dart
+++ lib/src/presentation/color_picker.dart
@@ -2,68 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:custom_color_picker/src/application/color_picker_controller.dart';
+import 'package:custom_color_picker/src/presentation/color_slider.dart';
 
 class ColorPicker extends ConsumerWidget {
   const ColorPicker({
     super.key,
     this.withOpacity = false,
+    this.withSaturation = false,
   });
 
   final bool withOpacity;
+  final bool withSaturation;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(colorPickerControllerProvider);
 
-    final color = HSVColor.fromAHSV(state.alpha, state.hue, 1.0, 1.0).toColor();
-
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         AnimatedContainer(
-          decoration: BoxDecoration(
-            border: Border.all(color: color, width: 2),
-            color: color,
-          ),
+          color: state.color.toColor(),
           duration: const Duration(milliseconds: 100),
           width: 200,
           height: 200,
         ),
         const SizedBox(height: 48.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Container(
-            height: 30.0,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  for (var h = 0.0; h < 360.0; h += 1)
-                    HSVColor.fromAHSV(1.0, h, 1.0, 1.0).toColor()
-                ],
-                stops: [
-                  for (var h = 0.0; h < 360.0; h += 1) h / 360.0,
-                ],
-              ),
-            ),
-          ),
-        ),
-        Slider.adaptive(
-          value: state.hue,
-          min: 0,
-          max: 360,
-          onChanged: (newValue) => ref
-              .read(colorPickerControllerProvider.notifier)
-              .changeHue(newValue),
-        ),
-        if (withOpacity)
-          Slider.adaptive(
-            value: state.alpha,
-            min: 0,
-            max: 1.0,
-            onChanged: (newValue) => ref
-                .read(colorPickerControllerProvider.notifier)
-                .changeOpacity(newValue),
-          ),
+        const ColorSlider(ColorPickerValue.hue),
+        if (withOpacity) const ColorSlider(ColorPickerValue.alpha),
+        if (withSaturation) const ColorSlider(ColorPickerValue.saturation),
       ],
     );
   }
---
+++ lib/src/presentation/color_slider.dart
@@ -0,0 +1,98 @@
+import 'package:custom_color_picker/src/application/color_picker_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+enum ColorPickerValue {
+  alpha,
+  hue,
+  saturation,
+}
+
+class ColorSlider extends ConsumerWidget {
+  const ColorSlider(
+    this.parameter, {
+    super.key,
+  });
+
+  final ColorPickerValue parameter;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(colorPickerControllerProvider);
+
+    late double value;
+    late List<Color> gradientColors;
+    late List<double> stops;
+    late Function(double) changeValue;
+
+    switch (parameter) {
+      case ColorPickerValue.alpha:
+        value = state.alpha;
+        gradientColors = [
+          for (var alpha = 0.0; alpha < 1.0; alpha += 0.01)
+            HSVColor.fromAHSV(alpha, 1.0, 1.0, 1.0).toColor()
+        ];
+        stops = [for (var alpha = 0.0; alpha < 1.0; alpha += 0.01) alpha];
+        changeValue = (newValue) => ref
+            .read(colorPickerControllerProvider.notifier)
+            .changeOpacity(newValue);
+        break;
+      case ColorPickerValue.hue:
+        value = state.hue;
+        gradientColors = [
+          for (var h = 0.0; h < 360.0; h += 1)
+            HSVColor.fromAHSV(1.0, h, 1.0, 1.0).toColor()
+        ];
+        stops = [for (var h = 0; h < 360.0; h += 1) h / 360.0];
+        changeValue = (newValue) => ref
+            .read(colorPickerControllerProvider.notifier)
+            .changeHue(newValue);
+        break;
+      case ColorPickerValue.saturation:
+        value = state.saturation;
+        gradientColors = [
+          for (var v = 1.0; v >= 0.0; v -= 0.01)
+            HSVColor.fromAHSV(1.0, 0.0, 0.0, v).toColor()
+        ];
+        stops = [for (var s = 0.0; s < 1.0; s += 0.01) s];
+        changeValue = (newValue) => ref
+            .read(colorPickerControllerProvider.notifier)
+            .changeSaturation(newValue);
+        break;
+    }
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                parameter.toString().split('.').last.toUpperCase(),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Container(
+            height: 30.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: gradientColors,
+                stops: stops,
+              ),
+            ),
+          ),
+        ),
+        Slider.adaptive(
+          value: value,
+          min: 0,
+          max: parameter == ColorPickerValue.hue ? 360 : 1.0,
+          onChanged: (newValue) => changeValue(newValue),
+        ),
+      ],
+    );
+  }
+}
